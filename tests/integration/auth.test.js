47c19bcd4966d5e3fc65f99cb943990a5b58a1cd
AMIRIX\chouinard <andre.chouinard@vemco.com>
2022-01-20 22:14:56 -0400

    Initial Check In

---
+++ tests/integration/auth.test.js
@@ -0,0 +1,48 @@
+const request = require('supertest');
+const { User } = require('../../models/user');
+const { Genre } = require('../../models/genre');
+
+describe('Auth Middleware', () =>{
+    
+    let token;
+    
+    beforeEach(() => { 
+        server = require('../../index');
+        token = new User().generateAuthToken();
+    });
+
+    afterEach(async () => { 
+        await Genre.remove({}); 
+        server.close(); 
+    });
+
+    const exec = function(){
+        return request(server)
+            .post('/api/genres/')
+            .set('x-auth-token', token)
+            .send({ name: 'genre1'});
+    };
+
+    it('should return 401 if no token is provided', async () => {
+        token = '';
+
+        const res = await exec();
+
+        expect(res.status).toBe(401);
+    });
+
+    it('should return 400 if invalid token is provided', async () => {
+        token = 'a';
+
+        const res = await exec();
+
+        expect(res.status).toBe(400);
+    });
+
+    it('should return 200 if valid token is provided', async () => {
+
+        const res = await exec();
+
+        expect(res.status).toBe(200);
+    });
+});
