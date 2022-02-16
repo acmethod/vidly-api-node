14216f22eb9eba848c51dc9818dda172869d5549
AMIRIX\chouinard <andre.chouinard@vemco.com>
2022-02-16 13:44:26 -0400

    remove unique attribute in genre schema

--- routes/movies.js
+++ routes/movies.js
@@ -26,6 +26,8 @@ router.get('/:id', async (req, res)=>{
 router.post('/', [auth, validate(validateMovie)], async (req, res)=>{
     const {error} = validate(req.body);
     
+    console.log(req);
+
     if (error){
         res.status(400).send(error.details[0].message);
         return;
@@ -37,6 +39,8 @@ router.post('/', [auth, validate(validateMovie)], async (req, res)=>{
         return;
     }
 
+    console.log(genre);
+
     const movie = new Movie({
         title: req.body.title,
         genre: {
@@ -46,6 +50,8 @@ router.post('/', [auth, validate(validateMovie)], async (req, res)=>{
         numberInStock: req.body.numberInStock,
         dailyRentalRate: req.body.dailyRentalRate
     });
+
+    console.log(movie);
 
     await movie.save();
 
